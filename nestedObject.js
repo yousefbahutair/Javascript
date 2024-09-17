173ffde9fbb318a557d663b0125e9c467400b27d
yousefbahutair <yousefnas06@gmail.com>
2024-09-17 14:47:35 +0400

    nestedObject

---
+++ nestedObject.js
@@ -0,0 +1,16 @@
+const studentFile = {
+    studentPersonalInformation : {
+        studentName: Yousef,
+        studentAge: 17,
+    } ,
+    studentAcadimic : {
+    studentMedtermExamDgree : 98,
+    studentFinalExamdgree : 99
+    },
+    studentAttendence : {
+
+        firstSimister : 2,
+        secondSimister :7
+    }
+
+};
