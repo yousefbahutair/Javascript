8b00709aeff16f0ac3d719029db1c9640c22f492
yousefbahutair <yousefnas06@gmail.com>
2024-09-19 13:51:03 +0400

    loops/objects

---
+++ Object3.js
@@ -0,0 +1,14 @@
+const xoGame  = {
+    playerOne : {
+    namePlayer : "yousef",
+    playerLevel : "40"  
+    },
+    playerTwo : {
+        namePlayer : "Ali",
+        playerLevel : "35"  
+        },
+    Board : [
+        [null,null,"x"],["o","x","o"],["x", "o", "x"]
+    ],
+
+};
---
+++ forLoops.js
@@ -0,0 +1,10 @@
+
+const arr1 = [
+     ["Ahmed ","Mohammed","Ali"],
+    [90,85,100],
+    ["A-", "B+" ,"A+"]
+];
+for( let i = 0 ; i <= arr1.length; i++)
+{
+    console.log(`Name of the student is : ${arr1[0][i]}, his mark: ${arr1[1][i]} , his grade ${arr1[2][i]} `)
+}
