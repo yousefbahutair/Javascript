a7a74ae5995f215718c26e1caf9ec249112d9f43
yousefbahutair <yousefnas06@gmail.com>
2024-10-01 14:31:46 +0400

    nestedFunctions

---
+++ nestedFunctions.js
@@ -0,0 +1,19 @@
+
+function choice(arr){
+
+ 
+
+    function avrage(){
+
+        function sum (){
+            let sum = 0 ;
+            for(num of arr){
+               sum =+ num
+        }
+        return sum;
+    }
+    let avrage = sum() / arr.length;
+    return avrage;
+}
+     return avrage()
+}
