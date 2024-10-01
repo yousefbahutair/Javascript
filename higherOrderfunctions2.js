100b08bdce5a29fb0e8925a07cd49709a6e1e012
yousefbahutair <yousefnas06@gmail.com>
2024-10-01 14:59:12 +0400

    higherOrderfunction 2

---
+++ higherOrderfunctions2.js
@@ -0,0 +1,11 @@
+function multiply(x,y){
+    return x * y
+}
+function subtract(x,y){
+    return x-y
+}
+function addtion(x,y){
+    return x + y
+}
+object = {doMultiply : multiply, doSubtract : subtract , doAddtion :addtion}
+
