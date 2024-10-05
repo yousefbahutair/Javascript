4569940b4302505ff3260c6700f460d27a36efe9
yousefbahutair <yousefnas06@gmail.com>
2024-10-05 14:02:35 +0400

    mapFunction

---
+++ mapFunction.js
@@ -0,0 +1,24 @@
+realNumbers = [
+    23,
+    34,
+    5357,
+    645,
+    5345,
+];
+const evenNums = { number : realNumbers.map(function(num){
+    return num;}),
+ 
+    type:
+    realNumbers.map(function(num){
+    if((num % 2) ===  0){
+        return "Even Number."
+    }
+    else if(num % 2 !== 0){
+        return "Odd Number."
+    }
+    else if (typeof(num) === typeof(1))
+    {
+        return "Not Number."
+    }
+})
+}
