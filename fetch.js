34cd99dcab0476c239dd15cbc595bd4e9773348b
yousefbahutair <yousefnas06@gmail.com>
2025-01-18 11:37:27 +0400

    Fixing Catch Error

--- fetch.js
+++ fetch.js
@@ -1,12 +1,15 @@
-fetch('https://swapi.dev/api/planets/')
+fetch('https://swapi.deeev/api/planets/')
 .then((res) => {
+    if(!res.ok){
+        throw new Error(`Something went wrong ${res.status}`)
+    } else{
     console.log('Request Sent!')
     res.json().then((data) => { 
         for(let planet of data.results){
             console.log(planet.name)
         }
     })
-})
+}})
 .catch((err) => {console.log(err.status)
     console.log("ERROR!!!")
 })
